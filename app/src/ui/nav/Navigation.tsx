b19a0d78c255cf1412b93c3ac84fa58a92908ee1
Lukasz <7lukasz8@gmail.com>
2024-04-23 20:09:03 +0200

    title

--- app/src/ui/nav/Navigation.tsx
+++ app/src/ui/nav/Navigation.tsx
@@ -4,7 +4,9 @@ import DarkModeButton from "./DarkModeBtn";
 export default function Navigation() {
   return (
     <div className="bg-bgWhite  w-full">
-      <p className="text-bgWhite dark:bg-bgDark bg-bgWhite">Navigation</p>
+      <p className="text-bgDark dark:text-bgWhite dark:bg-bgDark bg-bgWhite">
+        Navigation
+      </p>
       <DarkModeButton />
     </div>
   );
