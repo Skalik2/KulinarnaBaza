47eb31950d3c1b7923e76fb3eb0774ee379c0d9e
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-05-02 17:49:33 +0200

    Hashing + fixed console error

--- server/auth.ts
+++ server/auth.ts
@@ -2,6 +2,7 @@ import {Express} from "express";
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const pool = require("./db");
+const { hashPassword, comparePasswords } = require("./hashing");
 
 declare global {
     namespace Express {
@@ -24,17 +25,25 @@ module.exports = function(app : Express) {
         try {
             const { email, password } = req.body;
             console.log("Login request:\n", email, password);
-            // TODO: Change to hashed
+
             const result = await pool.query(
-                "SELECT * FROM public.uzytkownik WHERE email = $1 AND haslo = $2", [email, password]
+                "SELECT * FROM public.uzytkownik WHERE email = $1",
+                [email]
             );
-            //console.log(result.rowCount);
             if (result.rowCount != 1) {
-                console.error("Incorrect credentials!");
+                console.error("Incorrect email!");
                 res.status(500).json({ error: "Incorrect email and password combination!" });
                 return;
             }
-            req.session.userId = result.rows[0].id_uzytkownika;
+            else if (!comparePasswords(password, result.rows[0].haslo)) {
+                console.error("Incorrect password!");
+                res.status(500).json({ error: "Incorrect email and password combination!" });
+                return;
+            }
+
+            console.log("Correct password:",comparePasswords(password, result.rows[0].haslo));
+            console.log("Logged in successfully!")
+            req.session.user = email;
             res.status(256).json( {"response": "Logged in successfully!"} );
 
         }
@@ -46,7 +55,8 @@ module.exports = function(app : Express) {
 
     app.get("/api/logout", bodyParser.json(), async(req : any, res) => {
         try {
-            req.session.userId = null;
+            req.session.user = null;
+            req.session.authenticated = false;
             req.session.destroy()
             req.session = null;
             res.status(256).json( {"response": "Logged out successfully!"} );
@@ -61,23 +71,27 @@ module.exports = function(app : Express) {
         try {
             const { email, password, name, lastname } = req.body;
             console.log("Register request:\n", email, password, name, lastname);
-            // TODO: Change to hashed
-            const result1 = await pool.query(
-                "insert into public.uzytkownik (imie, nazwisko, haslo, email) values ($1, $2, $3, $4);", [name, lastname, password, email]
-            );
+            const hpass = await hashPassword(password);
+            
             const result2 = await pool.query(
-                "SELECT * FROM public.uzytkownik WHERE email = $1 AND haslo = $2", [email, password]
+                "SELECT * FROM public.uzytkownik WHERE email = $1",
+                [email]
             );
-            console.log(result2.rows);
-
-            if (result2.rowCount != 1) {
-                console.error("Incorrect credentials!");
-                res.status(500).json({ error: "Couldn't register!!" });
+            if (result2.rowCount == 1) {
+                console.error("ERROR: A user already exists");
+                res.status(500).json({ error: "ERROR: A user already exists" });
                 return;
             }
-            req.session.userId = result2.rows[0].id_uzytkownika;
+
+            const result1 = await pool.query(
+                "INSERT INTO public.uzytkownik (imie, nazwisko, haslo, email) VALUES ($1, $2, $3, $4);",
+                [name, lastname, hpass, email]
+            );
+        
+            req.session.authenticated = true;
+            req.session.user = email;
             res.status(256).json( {"response": "Registered successfully!"} );
-        }
+        }   
         catch (err) {
             console.error("Error when registering:", err);
             res.status(500).json({ error: "Error when registering!" });
