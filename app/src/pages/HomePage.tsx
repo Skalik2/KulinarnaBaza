31b53ffae86f18bbbd741ef2acbe8a544e3b53bd
karolmichonsky <karolmichonsky@gmail.com>
2024-05-04 22:43:18 +0200

    Feat: Prepare image module handling

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import hero from '../../images/hero.jpeg'
 
 export default function HomePage() {
   return (
-    <div className='text-main h-screen'>HomePage</div>
+    <div className='text-main '>
+      <img src={hero} alt="" />
+    </div>
   )
 }
