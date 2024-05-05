2267eb3fda6f798cb2dfd9298ec8af682f892ee8
karolmichonsky <karolmichonsky@gmail.com>
2024-05-05 19:55:13 +0200

    Feat: Create hero section

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
-import hero from '../../images/hero.jpeg'
+import hero from '../../images/hero.jpg'
+
+const heroImg = {
+  backgroundImage: `url(${hero})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  width: "100%"
+}
 
 export default function HomePage() {
   return (
-    <div className='text-main '>
-      <img src={hero} alt="" />
+    <div style={heroImg} className='text-main h-[calc(100vh-64px)] md:h-[calc(100vh-72px)] flex justify-center items-center'>
+      <h1 className=' md:w-1/3 text-6xl text-center text-white font-bold drop-shadow-xl select-none'>Baza posiłków z którą ugotujesz codzienny obiad</h1>
     </div>
   )
 }
