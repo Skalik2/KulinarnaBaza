c5d09fb71cb5140bb59c33b7fcb5541c5a7805a4
karolmichonsky <karolmichonsky@gmail.com>
2024-05-06 21:58:53 +0200

    Feat: Create footer

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Hero from '../ui/home/Hero'
 import Carousel from '../ui/home/Carousel'
+import Footer from '../ui/footer/Footer'
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       <Carousel />
+      <Footer />
     </div>
   )
 }
---
+++ app/src/ui/footer/Footer.tsx
@@ -0,0 +1,28 @@
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+export default function Footer() {
+  return (
+    <div className='flex flex-col flex-wrap'>
+            <div className=' flex flex-col flex-wrap bg-bgWhite mx-auto text-bgDark dark:bg-bgDark dark:text-bgWhite justify-center items-center text-center p-7 w-full'>
+                <h1 className=' text-3xl mx-auto'>KulinarnaBaza</h1>
+                <p className='p-3'>Najsmaczniejsze przepisy w jednym miejscu, poznaj nasze smaki!</p>
+                <div className='flex justify-center items-center [&>*]:mx-2'>
+                    <FaFacebook className='w-7 h-7'/>
+                    <FaLinkedin className='w-7 h-7'/>
+                    <FaSquareXTwitter className='w-7 h-7'/>
+
+                </div>
+            </div>
+            <div className=' bg-black text-white flex justify-between p-4'>
+                <p>Copyright ©</p>
+                <ul className='flex flex-row [&>*]:px-2'>
+                    <li>Przepisy</li>
+                    <li>Artykuły</li>
+                    <li>Plan posiłków</li>
+                    <li>Ranking</li>
+                </ul>
+            </div>
+        </div>
+  )
+}
