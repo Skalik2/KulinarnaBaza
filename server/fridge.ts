217e38835d541c2c81004dcd0d46af86dc205606
jmisina <jmisina02@gmail.com>
2024-05-16 11:00:32 +0200

    Fridge ingredients DELETE method added

--- server/fridge.ts
+++ server/fridge.ts
@@ -23,16 +23,23 @@ module.exports = function (app: Express) {
 
   app.post("/api/fridge/:userId", bodyParser.json(), async (req: any, res) => {
     try {
-      
+      let errList = []  
       console.log(req.body);
-      
-      const result = await pool.query(
-        `INSERT INTO skladnik_w_lodowce VALUES (${req.body.ingredientId}, ${req.params.userId}, ${req.body.amount})`
-        
-      );
+      const input = req.body.skladniki
+      for (let i =0; i < input.length; i++){
+      let valid = await pool.query(`SELECT * FROM skladnik_w_lodowce WHERE id_skladnik = ${input[i].id_skladnik} and id_uzytkownika = ${req.params.userId}`)
+      console.log(valid)
+      if (valid.rowCount === 0){
+      await pool.query(
+        `INSERT INTO skladnik_w_lodowce VALUES (${input[i].id_skladnik}, ${req.params.userId}, ${input[i].ilosc})`
+      );}
+      else {
+        errList.push(input[i].id_skladnik)
+      }
+    }
 
-      console.log(`Successfully added ${req.body.ingredientId} to ${req.params.userId}'s fridge`);
-      res.status(200).json({ response: "Ingredients added successfully!" });
+      console.log(`Successfully added ingredients to user ${req.params.userId}'s fridge. Ingredients already in fridge (not added): ${errList}`);
+      res.status(200).json({ response: `Ingredients added successfully! Not added: ${errList}` });
     } 
     catch (err) {
       console.error("Adding ingredients failed", err);
@@ -56,6 +63,25 @@ module.exports = function (app: Express) {
     catch (err) {
       console.error("Fridge GET method fail", err);
       res.status(500).json({ error: "Fridge GET method fail" });
+    }
+  });
+
+  app.delete("/api/fridge/:userId", bodyParser.json(), async (req: any, res) => {
+    try {
+      
+      console.log(req.body);
+      
+      const result = await pool.query(
+        `DELETE FROM skladnik_w_lodowce WHERE id_uzytkownika = ${req.params.userId} and id_skladnik = ${req.body.id_skladnik}`
+        
+      );
+
+      console.log(`User ${req.params.userId}'s DELETE from fridge success`);
+      res.status(200).json({ response: "essa byku" });
+    } 
+    catch (err) {
+      console.error("Fridge DELETE method fail", err);
+      res.status(500).json({ error: "Fridge DELETE method fail" });
     }
   });
 };
