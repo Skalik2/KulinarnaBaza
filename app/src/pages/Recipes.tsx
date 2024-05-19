6ffe4a477e9779e6e87a2ef92cbf177097619b5a
karolmichonsky <karolmichonsky@gmail.com>
2024-05-19 21:06:35 +0200

    Feat: Prototype API usage for pre alpha test

--- app/src/pages/Recipes.tsx
+++ app/src/pages/Recipes.tsx
@@ -1,6 +1,33 @@
 import RecipeCard from "../ui/recipes/RecipeCard";
+import { useState, useEffect } from "react";
 
 export default function Recipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/recipes", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        //credentials: "include", not needed ig
+      });
+  
+      if (!response.ok) {
+        throw new Error("Error in fetching recipes");
+      }
+  
+      const result = await response.json();
+      //setRecipes(result);
+    } catch (error) {
+      console.error("Error when handling the GET request:", error);
+    }
+  };
   return (
     <div className="flex flex-wrap p-16 justify-center gap-4 dark:bg-bgDark">
       <RecipeCard imageSrc="../../images/hero.jpg" title="Przepis" />
