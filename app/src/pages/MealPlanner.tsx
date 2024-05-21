c7061de61c348dc2c2ed5a1ab6e89637798f75ff
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-05-21 23:09:46 +0200

    Meal planner get meals api

--- app/src/pages/MealPlanner.tsx
+++ app/src/pages/MealPlanner.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import Days from "../ui/mealPlanner/days";
+import RecipeCard from "../ui/recipes/RecipeCard";
+import axios from "axios";
 
 export default function MealPlanner() {
+  const [planner, setPlanner] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const userId = 34;
+
+  useEffect(function () {
+    axios.get(`http://localhost:5000/api/mealplanner/${userId}`).then((res) => {
+      setPlanner(res.data.response);
+    });
+  }, []);
+
+  useEffect(function () {
+    planner.forEach(element => {
+      axios.get(`http://localhost:5000/api/recipes/${element.id_przepisu}`).then((res) => {
+        console.log(res.data)
+        // Dodawanie kart przepisów do listy
+      });
+    });  
+  }, [planner]);
+  
+  console.log("recipes",recipes);
+  
+
   return (
-    <div className=" bg-bgWhite text-bgDark dark:bg-bgDark dark:text-bgWhite flex flex-row justify-center">
-      <Days />
+    <div className="bg-bgWhite text-bgDark dark:bg-bgDark dark:text-bgWhite">
+      <div className="flex justify-center">
+        <Days />
+      </div>
+      <div className="flex justify-center">
+        {/* karty przepisów */}
+      </div>
     </div>
   );
 }
