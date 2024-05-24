519a3a424e3d7bbcd3e80936d9d4505106eed425
karolmichonsky <karolmichonsky@gmail.com>
2024-05-24 22:25:48 +0200

    Fix: Hero section text

--- app/src/ui/home/Hero.tsx
+++ app/src/ui/home/Hero.tsx
@@ -10,8 +10,9 @@ const heroImg = {
 
 export default function Hero() {
   return (
-    <div style={heroImg} className='text-main h-screen flex justify-center items-center'>
-      <h1 className=' md:w-1/3 text-6xl text-center text-white font-bold drop-shadow-xl select-none'>Baza posiłków z którą ugotujesz codzienny obiad</h1>
+    <div style={heroImg} className='text-main h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-6xl text-center text-white font-bold drop-shadow-xl select-none'>Codzienny obiad</h1>
+      <h1 className='text-4xl text-center text-white font-bold drop-shadow-xl select-none'>W kulinarnej bazie</h1>
     </div>
   )
 }
