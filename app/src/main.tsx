a1ae9100a4b3dd1147159400f01433213555162d
Lukasz <7lukasz8@gmail.com>
2024-05-31 19:29:01 +0200

    error boundler

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorPage from "./pages/ErrorPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ErrorBoundary
+      FallbackComponent={ErrorPage}
+      onReset={() => {
+        window.location.replace("/");
+      }}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
