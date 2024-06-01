e35a92bc08d23c969be6d9e04e46a2e5215b7852
karolmichonsky <karolmichonsky@gmail.com>
2024-06-01 22:22:01 +0200

    Feat: Prepare profile calls

--- app/src/pages/Account.tsx
+++ app/src/pages/Account.tsx
@@ -1,10 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { GiCook } from "react-icons/gi";
 import { useUser } from "../hooks/useUser";
 import { useGetUserRecipes } from "../hooks/useGetUserRecipes";
 import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import FormInput from "../ui/FormInput";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { DarkModeContext } from "../context/DarkModeContext";
+import axios from "axios";
+import toast from "react-hot-toast";
+axios.defaults.withCredentials = true;
 
 export default function Account() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>();
+  const { isDarkMode } = useContext(DarkModeContext);
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    axios
+      .post(
+        "http://localhost:5000/api/user/changePassword",
+        {
+          password: data.password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then(() => {
+        window.location.href = "/";
+      })
+      .catch(() => {
+        toast.error("Podaj poprawne dane");
+      });
+  };
+
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { data: userData, isLoading: userIsLoading } = useUser();
@@ -17,26 +50,63 @@ export default function Account() {
     if (recipeData) {
       setRecipeCount(recipeData.length);
     }
-  }, [recipeData]);
+  }, []);
 
   if (userIsLoading || recipeIsLoading) {
     return <div>Loading...</div>;
   }
 
   if (!queryClient.getQueryData(["user"])) navigate("/login");
-  // useEffect(
-  //   function () {
-  //   },
-  //   [navigate, queryClient]
-  // );
 
   return (
-    <div className="pt-16 md:pt-[72px] dark:bg-bgDark flex flex-col items-center justify-center text-black dark:text-white h-screen">
+    <div className="pt-16 md:pt-[72px] dark:bg-bgDark flex flex-col items-center justify-around text-black dark:text-white h-screen text-xl">
       <h1 className="text-3xl font-semibold">Moje konto</h1>
-      <p>
-        Witaj {userData?.imie} {userData?.nazwisko}
-      </p>
-      <p>Opublikowałeś {recipeCount} przepisów.</p>
+      <div className=" flex flex-col justify-around items-center [&>*]:py-1">
+        <GiCook
+          className=" text-main rounded-full border-4 border-main"
+          size={60}
+        />
+        <p>
+          Witaj {userData?.imie} {userData?.nazwisko}
+        </p>
+        <p>Opublikowałeś {recipeCount} przepisów.</p>
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 md400:gap-6 md600:gap-8 xs:w-full xs:px-10 md500:w-[450px] "
+      >
+        <p className="text-center">Zmień hasło</p>
+        <div className="w-full">
+          <FormInput
+            id="oldPassword"
+            type="password"
+            label="Obecne hasło"
+            error={errors?.password?.message}
+            register={register}
+          />
+        </div>
+        <div className="">
+          <FormInput
+            id="newPassword"
+            type="password"
+            label="Nowe hasło"
+            error={errors?.password?.message}
+            register={register}
+          />
+        </div>
+        <div className="mb-6">
+          <FormInput
+            id="newRepPassword"
+            type="password"
+            label="Powtórz nowe hasło"
+            error={errors?.password?.message}
+            register={register}
+          />
+        </div>
+        <button className="w-full bg-main hover:bg-mainHover py-2 text-white uppercase tracking-widest font-light rounded-full transition-all duration-300">
+          Zmień hasło
+        </button>
+      </form>
     </div>
   );
 }
